3a780410b5e426775cc57cfc1be4f85cfe5de726
kyang96 <e0310998@u.nus.edu>
2019-08-26 12:09:22 +0800

    Added support to track 3 types of tasks: Todo, Deadlines and Events

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,38 +15,43 @@ public class Duke {
         printLine();
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
         List<Task> taskList = new ArrayList<Task>();
-        while (!str.equals("bye")){
-            if (str.equals("list")){
+        String str;
+        do {
+            str = sc.nextLine();
+            String[] strArr = str.split(" ",2);
+            if (strArr[0].equals("list")){
                 printLine();
                 int count = 1;
                 System.out.println("Here are the tasks in your list:");
                 for (Task t : taskList){
-                    System.out.println(count + ".[" + t.getStatusIcon() + "] " + t.getDescription());
+                    System.out.println(count + "." + t.toString());
                     count++;
                 }
                 printLine();
-            } else if (str.length() >= 5 && str.substring(0,4).equals("done")) {
-                String[] strArr = str.split(" ");
+            } else if (strArr[0].equals("done")) {
                 int num = Integer.parseInt(strArr[1]) - 1;
                 Task task = taskList.get(num);
                 task.markAsDone();
                 taskList.set(num, task);
                 printLine();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.println(task.toString());
                 printLine();
-            }
-            else {
+            } else if (strArr[0].equals("todo") || strArr[0].equals("deadline") || strArr[0].equals("event")) {
                 printLine();
-                Task task = new Task(str);
+                Task task = addTask(strArr[0], strArr[1]);
                 taskList.add(task);
-                System.out.println("added: " + str);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(task.toString());
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                printLine();
+            } else {
+                printLine();
+                System.out.println("Invalid command");
                 printLine();
             }
-            str = sc.nextLine();
-        }
+        } while (!str.equals("bye"));
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
@@ -54,5 +59,19 @@ public class Duke {
 
     private static void printLine(){
         System.out.println("____________________________________________________________");
+    }
+
+    private static Task addTask(String type, String description){
+        String[] info;
+        if (type.equals("todo")){
+            return new Todo(description);
+        } else if (type.equals("deadline")){
+            info = description.split("/by");
+            return new Deadline(info[0].trim(), info[1].trim());
+        } else if (type.equals("event")){
+            info = description.split("/at");
+            return new Event(info[0].trim(), info[1].trim());
+        }
+        return null;
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -22,4 +22,8 @@ public class Task {
     public void setDescription(String description){
         this.description = description;
     }
+
+    public String toString(){
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
 }
